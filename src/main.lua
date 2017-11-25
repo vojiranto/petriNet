40d91e07d7b7a36fccf014a44b808d68612c4093
dmitriy.pavlyuk <al.lastor@bk.ru>
2017-11-26 00:05:04 +0300

    Правка

--- src/main.lua
+++ src/main.lua
@@ -2,29 +2,18 @@ function new.Main ()
     local private = {}
     local public  = {}
     
-    public.run = function ()
-        private.knots      = {new.Knot(1), new.Knot(1), new.Knot(1)}
-        private.converters = {new.Converter(), new.Converter()}
-        
-        -- ставим маркер в начало цепочки
-        --[[
-        private.knots[1].addContent(1)
-        
-        private.converters[1].addInputKnot(private.knots[1], 1)
-        private.converters[1].addOutputKnot(private.knots[2], 1)
-
-        private.converters[2].addInputKnot(private.knots[2], 1)
-        private.converters[2].addOutputKnot(private.knots[3], 1)
-    
-        private.petriNet = new.PetriNet(private.knots, private.converters)
-        --]]
+    public.run = function ()        
+        -- узлы и преобразователи
         private.petriNet = new.PetriNet{
             {{1}, {2}},
             {{2}, {3}}
+            {{3}, {}}
         }
+        
+        -- маркеры
         private.petriNet.addContent{{1,1}}
         
-        for i = 1, 3 do
+        for i = 1, 4 do
             private.petriNet.print()
             print "-----"
             private.petriNet.step()
@@ -33,7 +22,3 @@ function new.Main ()
 
     return copy(public)
 end
---[[
-{{1}, {2}}
-{{2}, {3}}
---]]
