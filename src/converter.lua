7da47143c2235de55ae95ac6646c161fa79c085c
dmitriy.pavlyuk <al.lastor@bk.ru>
2017-11-25 18:54:51 +0300

    Добавлено описание преобразователя, поправлено описание узла

---
+++ src/converter.lua
@@ -0,0 +1,45 @@
+function new.Converter ()
+    local private {
+        inputKnots  = {},
+        outputKnots = {},
+        isActive      = false,
+    }
+    private.addKnot = function (knotTable, knot, multiplicity)
+        knotTable[knot] = multiplicity,
+        }
+    end
+
+    local public = {}
+    
+    public.addInputKnot = function (knot, multiplicity)
+        private.addKnot(private.inputKnots, knot, multiplicity)
+    end
+    
+    public.addOutputKnot = function (knot, multiplicity)
+        private.addKnot(private.outputKnots, knot, multiplicity)
+    end
+
+
+    public.take = function ()
+        -- проверить, что взятие возможно
+        for k, v in pairs(private.inputKnots) do
+            if not k.enoughContent(v) then
+                private.isActive = false
+                return
+        end end
+        
+        -- взять всё что нужно
+        for _, v in pairs(private.inputKnots) do
+            k.getContent(v)
+        end
+        private.isActive = true
+    end
+    
+    -- разложить всё, что нужно
+    public.recoil = function ()
+        for k, v in pairs(private.outputKnots) do
+            k.addContent(v)
+    end end
+
+    return copy(public)
+end
--- src/knot.lua
+++ src/knot.lua
@@ -1,39 +1,39 @@
-function Knot (limit)
-    local privat = {
+function new.Knot (limit)
+    local private = {
         content = 0,
         limit   = limit,
     }
     local public = {}
     
     -- удалить излишек 
-    privat.removeExcess = function ()
-        if privat.content > limit then
-            local rest = privat.content - limit
-            privat.content = limit
+    private.removeExcess = function ()
+        if private.content > limit then
+            local rest = private.content - limit
+            private.content = limit
             return rest    
     end end
 
     -- установить ограничение
     public.setLimit = function (limit)
-        privat.limit = limit
-        return privat.removeExcess()
+        private.limit = limit
+        return private.removeExcess()
     end
     
     -- добавить содержимого
     public.addContent = function (content)
-        privat.content = privat.content + content
-        return privat.removeExcess()
+        private.content = private.content + content
+        return private.removeExcess()
     end
     
     -- содержимое существует в нужном количестве
     public.enoughContent = function (content)
-        return privat.content >= content
+        return private.content >= content
     end
     
     -- извлекаем содержимое, если его достаточно.
     public.getContent = function (content)
-        if privat.content >= content then
-            privat.content = privat.content - content
+        if private.content >= content then
+            private.content = private.content - content
             return content
     end end
     
